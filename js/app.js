871f3c29ad5430be9b2deb23d20acc040465d1d6
TowsifHasan <towsiafehasan007@gmail.com>
2021-09-02 15:07:40 +0600

    third commit

--- js/app.js
+++ js/app.js
@@ -1,32 +1,44 @@
+// search place 
 const searchBook = ()=> {
     const searchField = document.getElementById('search-field')
     const searchText = searchField.value;
-    // console.log(searchText);
     searchField.value = '';
-    const url = `http://openlibrary.org/search.json?q=${searchText}`
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
     .then(res => res.json())
-    .then(data => displaySearchResult(data.docs[0].seed))   
+    .then(data => displaySearchResult(data.docs))   
 }
 
 const displaySearchResult = books =>{
-    
+     console.log(books)
     const searchResult = document.getElementById('search-result');
-    books.forEach(book =>{
-        console.log(book);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = 
-        `
-        <div class="card h-100">
-            <img src="..." class="card-img-top" alt="...">
-         <div class="card-body">
-             <h5 class="card-title">${book.docs[0].title}</h5>
-         </div>
-       </div>
-        `;
-        searchResult.appendChild(div)
-    })
+    const errorResult = document.getElementById('error')
+    if(books.length === 0){
+        error.innerHTML= '<p>There is no result found.Please search something meaningful</p>'
+    }
+    else{
+        books.forEach(book =>{
+        
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = 
+            `
+            <div class="card h-100">
+                <img src="" class="card-img-top" alt="...">
+             <div class="card-body">
+                 <h5 class="card-title">Books Name: ${book.title}</h5>
+                 <h5 class="card-title">Author Name: ${book.author_name[0]}</h5>
+                 <h5 class="card-title">Publisher Name: ${book.publisher[0]}</h5>
+                 <h5 class="card-title">First Publish Year: ${book.first_publish_year}</h5>
+             </div>
+           </div>
+            `;
+            searchResult.appendChild(div)
+            searchResult.appendChild(errorResult)
+        })
+    }
+
+
     
 }
 
@@ -39,7 +51,7 @@ const displaySearchResult = books =>{
 
 
 
-
+// data.docs
 
 // searchBook('title')
 
